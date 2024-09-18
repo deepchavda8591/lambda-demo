b362aaca4c8992b3b696e55bcaf2cbddf9a3ca61
deepchavda8591 <160210207+deepchavda8591@users.noreply.github.com>
2024-09-18 11:17:17 +0530

    Add files via upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,9 @@
 import json
+from test import two_num_sum
+
+print(f"two_num_sum = {two_num_sum()}")
 
 
 def lambda_handler(event, context):
-    print("Deep Chavda :)")
-    print(json.dumps({"event": "Request Data", "details": "deep chavda ML engineer"}))
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    print(json.dumps({"event": "Request Data", "details": "deep"}))
+    return {"statusCode": 200, "body": json.dumps("Deep Chavda :)")}
---
+++ test.py
@@ -0,0 +1,2 @@
+def two_num_sum():
+    print(f"two_num_sum = {1+1}")
