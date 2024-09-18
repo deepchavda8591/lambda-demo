cb7413a917f61a168b50f037e774cee544188dec
deepchavda8591 <160210207+deepchavda8591@users.noreply.github.com>
2024-09-18 10:54:41 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,6 @@ import json
 
 
 def lambda_handler(event, context):
+    print("Deep Chavda :)")
     print(json.dumps({"event": "Request Data", "details": "deep chavda ML engineer"}))
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
